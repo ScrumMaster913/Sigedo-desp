22723f318ea6ac73d22dc2e9558789a4d7bea32a
Jhon Kevin <yon.unjbg13@gmail.com>
2018-08-06 01:02:13 -0500

    Avance hasta el tuto 3 - creacion de rutas laravel

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,17 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Home';
+});
+
+Route::get('/usuarios/{id}',function($id){
+	return "Mostrando el detalle del usuario:  {$id}";
+})->where('id','[0-9]+');
+
+Route::get('/usuarios/nuevo',function(){
+	return 'Crear nuevo usuario';
+});
+
+Route::get('/saludo/{name}/{nickname}',function($name,$nickname){
+	return "Bienvenido {$name} , tu apodo es {$nickname} ";
 });
